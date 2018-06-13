a4f61c2db0c85ef2abbe03ac13ac2339400c7f8e
haoge <470368500@qq.com>
2018-06-13 11:15:56 +0800

    remove EasyThread library

--- app/src/main/java/com/haoge/sample/easyandroid/activities/mvp/MVPDemoActivity.kt
+++ app/src/main/java/com/haoge/sample/easyandroid/activities/mvp/MVPDemoActivity.kt
@@ -1,12 +1,12 @@
 package com.haoge.sample.easyandroid.activities.mvp
 
 import android.os.Bundle
+import com.haoge.easyandroid.easy.EasyExecutor
 import com.haoge.easyandroid.easy.EasyToast
 import com.haoge.easyandroid.mvp.MVPPresenter
 import com.haoge.easyandroid.mvp.MVPView
 import com.haoge.sample.easyandroid.R
-import com.lzh.easythread.AsyncCallback
-import com.lzh.easythread.EasyThread
+import java.util.concurrent.Callable
 
 /**
  * @author haoge on 2018/5/30
@@ -34,29 +34,27 @@ interface DemoView:MVPView {
 
 // P层数据处理：处理与界面无关的、与数据处理相关的逻辑。并用于连接M与V层的中间件
 class DemoPresenter(view:DemoView):MVPPresenter<DemoView>(view) {
-    val executor: EasyThread by lazy { EasyThread.Builder.createSingle().build() }
+    val executor: EasyExecutor by lazy { EasyExecutor.newBuilder(1)
+            .onError { _, _ ->
+                if (!isViewAttached()) return@onError
+                view.hideLoadingDialog()
+            }
+            .build() }
 
     fun requestData() {
         view?.showLoadingDialog()
 
         // 使用线程池模拟网络请求环境。
-        executor.async({
+        executor.async(Callable {
             Thread.sleep(3000)
-            return@async "Hello world"
-        }, object : AsyncCallback<String>{
-            override fun onSuccess(t: String?) {
-                // 对于异步网络操作，回调时尽量先判断一下是否处于View绑定状态。
-                if (!isViewAttached()) return
+            return@Callable "Hello world!"
+        }) {
+            // 对于异步网络操作，回调时尽量先判断一下是否处于View绑定状态。
+            if (!isViewAttached()) return@async
 
-                view?.hideLoadingDialog()
-                view?.onQuerySuccess(t)
-            }
-
-            override fun onFailed(t: Throwable?) {
-                view?.hideLoadingDialog()
-                view?.toastMessage("获取数据失败：Cause by -> ${t?.message?:"未知错误"}")
-            }
-        })
+            view?.hideLoadingDialog()
+            view?.onQuerySuccess(it)
+        }
     }
 
 }
